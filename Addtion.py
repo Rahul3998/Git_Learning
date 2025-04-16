91f8ccd7bb22dc30a20dfa8ba242cf8b54055542
Rahul3998 <rahuladagale3998@gmail.com>
2025-04-16 10:28:02 +0530

    branch-01 Addtion.py code updated

--- Addtion.py
+++ Addtion.py
@@ -4,3 +4,5 @@ def addition(num1,num2):
 num1 = 20
 num2 = 21
 print(f"Result for {num1} and {num2} = {addition(num1,num2)}")
+print(f"Result for {num1} and {num2} = {addition(num1,num2)}")
+print(f"Result for {num1} and {num2} = {addition(num1,num2)}")
